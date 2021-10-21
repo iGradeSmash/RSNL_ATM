784a7c4a3ec92c14d73ce6f35defc06fc0c38d64
Lenice1 <77759001+Lenice1@users.noreply.github.com>
2021-10-20 22:28:28 -0500

    Updated Files

--- Checkings.h
+++ Checkings.h
@@ -1,5 +1,17 @@
 #pragma once
-class Checkings
+#include "Account.h"
+
+class Checkings : public Account  //inheritence from base class 'account'
 {
+private:
+	double c_balance = 0;
+
+public:
+	void setBalance(double deposit);
+	double getBalance() const;
+	void setWithdraw();
+	void setDeposit();
+	double getTransfer();
+
 };
 
--- Savings.h
+++ Savings.h
@@ -1,5 +1,18 @@
 #pragma once
-class Savings
+#include "Account.h"
+
+class Savings : public Account  //inheritence from base class 'account'
 {
+private:
+	double s_balance = 0;
+
+public:
+	void setSavingsBalance(double Balance);
+	double getSavingsBalance() const;
+	void setWithdraw();
+	void setDeposit();
+	double getTransfer();
+
 };
+
 
